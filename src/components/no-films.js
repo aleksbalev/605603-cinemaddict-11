af7b548ff863d7d1c4e6f2a824f27d4cecd19148
AleksBalev <aleks.balev7899@gmail.com>
2020-04-21 19:30:20 +0200

    доделал фичу появления текста о отсутсвии фильмов, если таковые не поступают в распоряжение

---
+++ src/components/no-films.js
@@ -0,0 +1,31 @@
+import {
+  createElement
+} from '../utils';
+
+const createNoFilmsTemplate = () => {
+  return `<h2 class="films-list__title">There are no movies in our database</h2>`;
+};
+
+class NoFilms {
+  constructor() {
+    this._element = null;
+  }
+
+  getTemplate() {
+    return createNoFilmsTemplate();
+  }
+
+  getElement() {
+    if (!this._element) {
+      this._element = createElement(this.getTemplate());
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
+
+export default NoFilms;
